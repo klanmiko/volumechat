6abdb16a08b13c13b7298f6bcfce178bab16bcd8
klanmiko <eternity01033@gmail.com>
2015-06-14 21:08:22 +0800

    Revert f684696..df47e12
    This rolls back to commit f6846965aa0ead31dd790713940cfb55fca52a37.

--- server/main.js
+++ server/main.js
@@ -1,65 +1,57 @@
-var scene = {
-    objects: []
-};
+var scene={objects:[]};
 var http = require('http');
 var url = require('url');
 var maxheight = 1000;
 var maxwidth = 1000;
-var server = http.createServer(function(req, res) {
-    var msg = url.parse(req.url, true).query;
-    var returnval = {
-        x: 0,
-        y: 0,
-        shape: msg.icon,
-        name: msg.name
-    };
-    returnval.x = Math.round(Math.random() * maxwidth);
-    returnval.y = Math.round(Math.random() * maxheight);
+var server = http.createServer(function (req,res){
+    var msg = url.parse(req.url,true).query;
+    var returnval = {x:0,y:0,shape:msg.icon,name:msg.name};
+    returnval.x = Math.round(Math.random()*maxwidth);
+    returnval.y = Math.round(Math.random()*maxheight);
     scene.objects.push(returnval);
-    wss.broadcast(JSON.stringify(returnval));
     console.log(msg);
-    req.on('error', function() {
+    req.on('error',function(){
         console.error("error bitch in login");
     });
     res.writeHead(200, {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*"
     });
     res.write(JSON.stringify(returnval));
     res.end();
-    console.log(scene);
-
+        console.log(scene);
+    
 });
 server.listen(784);
-var WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({
-        port: 783
-    });
+var WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({ port: 783 });
 wss.broadcast = function broadcast(data) {
-    wss.clients.forEach(function each(client) {
-        client.send(data);
-    });
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
 };
 wss.on('connection', function connection(ws) {
     console.log("someone connected");
     console.log(scene);
-    ws.on('message', function incoming(message) {
-        try {
-            var msg = JSON.parse(message);
-            for (var x = 0; x < scene.objects.length; x++) {
-                if (scene.objects[x].name == msg.name) {
-                    scene.objects[x] = msg;
-                }
-            }
-            wss.broadcast(JSON.stringify(msg));
-        } catch (error) {
-
-        }
-
-    });
-    ws.send(JSON.stringify(scene));
+  ws.on('message', function incoming(message) {
+      try{
+        var msg = JSON.parse(message);
+      }
+      catch(error)
+      {
+          
+      }
+   for(var x = 0; x<scene.objects.length;x++)
+   {
+       if(scene.objects[x].name == msg.name)
+       {
+           scene.objects[x]=msg;
+       }
+   }
+  });    
+  ws.send(JSON.stringify(scene));
 });
-/*setInterval(function(){
+setInterval(function(){
     
     wss.broadcast(JSON.stringify(scene));
-},10);*/
+},10);
