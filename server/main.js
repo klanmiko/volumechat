4c675e8216dd4cd7938ace912bb739eed27d97b7
klanmiko <eternity01033@gmail.com>
2017-01-04 19:46:24 -0800

    mixed content errors

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 var server = require("./httpserver.js");
 var httpserver = server.createServer();
-console.log(process.env.port);
+console.log("port: "+process.env.PORT);
 httpserver.listen(process.env.PORT||8080);
 var ws = require("./gameserver.js")(httpserver);
 var chatserver = require("./chatservice.js")(httpserver);
