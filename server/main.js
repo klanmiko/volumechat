91b3516f2416c1b7379106648c7f6687687a78f0
klanmiko <eternity01033@gmail.com>
2017-01-04 19:37:57 -0800

    PORT not port

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 var server = require("./httpserver.js");
 var httpserver = server.createServer();
 console.log(process.env.port);
-httpserver.listen(process.env.port||8080);
+httpserver.listen(process.env.PORT||8080);
 var ws = require("./gameserver.js")(httpserver);
 var chatserver = require("./chatservice.js")(httpserver);
